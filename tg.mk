200c370e36d617f51e8faf7eafe6d9771c7da42b
arrrghhh <arrrghhh@xdandroid.com>
2014-04-29 21:17:46 -0600

    Typo

--- tg.mk
+++ tg.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/gummy/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/Gummy/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_DEVICE := mondrianwifi
 PRODUCT_NAME := tg_mondrianwifi
